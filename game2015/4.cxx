7bc5be154fc67ab0c1f8fb6e520a1ab38c8e3eb7
Rongkun Wang <elviswang011@gmail.com>
2023-12-30 19:15:42 +0100

    2015 4

--- game2015/4.cxx
+++ game2015/4.cxx
@@ -1,14 +1,31 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
+
+#include <openssl/md5.h>
 
 int main(int argc, char** argv) {
 
-  auto toPair = [](const housePos p){ return std::make_pair(p.real(), p.imag());};
 
-  std::ifstream inFile("game2015/4.txt");
-  std::string line;
-  std::getline(inFile, line);
+  unsigned char result[MD5_DIGEST_LENGTH];
+
+  std::string line("iwrupvqb");
+
+  int i(-1), out(0);
+  int sol1 = 0;
+  do {
+    std::stringstream ss;
+    ++i;
+    ss << line << i;
+    auto example = ss.str();
+    MD5((unsigned char*)example.c_str(), example.size(), result);
+    out = (static_cast<int>( result[0] ) << 16) + (static_cast<int>( result[1] ) << 8) + static_cast<int>( result[2] );
+    if (sol1 == 0 && (out >> 4) == 0) sol1 = i;
+  } while(out != 0);
+
+  std::cout << sol1 << std::endl;
+  std::cout << i << std::endl;
 
   return 0;
 }
